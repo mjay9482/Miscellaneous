504eac59d1c218e6ba736d1c5e5b9dab01f9f310
Mrityunjay <112894292+mjay9482@users.noreply.github.com>
2023-12-18 04:56:11 +0530

    Update RRT.py

--- RRT.py
+++ RRT.py
@@ -11,27 +11,17 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 import random
 
-# Load the image and perform preprocessing
-img = Image.open('cspace.png')
-img = img.convert('L')  # Convert to grayscale
-np_img = np.array(img)
-np_img = ~np_img  # Invert B&W
-np_img[np_img > 0] = 1
-
-# Save the preprocessed image
-np.save('cspace.npy', np_img)
-
-class TreeNode:
-    def __init__(self, locationX, locationY):
-        self.locationX = locationX
-        self.locationY = locationY
+class Node:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
         self.children = []
         self.parent = None
 
-class RRTalgo:
+class RRTAlgorithm:
     def __init__(self, start, goal, num_iterations, grid, step_size):
-        self.random_tree = TreeNode(start[0], start[1])
-        self.goal = TreeNode(goal[0], goal[1])
+        self.random_tree = Node(start[0], start[1])
+        self.goal_node = Node(goal[0], goal[1])
         self.nearest_node = None
         self.iterations = min(num_iterations, 300)
         self.grid = grid
@@ -41,17 +31,17 @@ class RRTalgo:
         self.path_distance = 0
         self.waypoints = []
 
-    def unit_vector(self, location_start, location_end):
-        v = np.array([location_end[0] - location_start.locationX, location_end[1] - location_start.locationY])
+    def unit_vector(self, start_node, end_node):
+        v = np.array([end_node.x - start_node.x, end_node.y - start_node.y])
         u_hat = v / np.linalg.norm(v)
         return u_hat
 
-    def add_child(self, location_x, location_y):
-        if location_x == self.goal.locationX:
-            self.nearest_node.children.append(self.goal)
-            self.goal.parent = self.nearest_node
+    def add_child(self, x, y):
+        if x == self.goal_node.x:
+            self.nearest_node.children.append(self.goal_node)
+            self.goal_node.parent = self.nearest_node
         else:
-            temp_node = TreeNode(location_x, location_y)
+            temp_node = Node(x, y)
             self.nearest_node.children.append(temp_node)
             temp_node.parent = self.nearest_node
 
@@ -61,22 +51,21 @@ class RRTalgo:
         point = np.array([x, y])
         return point
 
-    def steer_to_point(self, location_start, location_end):
-        offset = self.rho * self.unit_vector(location_start, location_end)
-        point = np.array([location_start.locationX + offset[0], location_start.locationY + offset[1]])
+    def steer_to_point(self, start_node, end_point):
+        offset = self.rho * self.unit_vector(start_node, end_point)
+        point = np.array([start_node.x + offset[0], start_node.y + offset[1]])
         if point[0] >= grid.shape[1]:
             point[0] = grid.shape[1] - 1
         if point[1] >= grid.shape[0]:
             point[1] = grid.shape[0] - 1
         return point
 
-    def is_in_obstacle(self, location_start, location_end):
-        u_hat = self.unit_vector(location_start, location_end)
+    def is_in_obstacle(self, start_node, end_point):
+        u_hat = self.unit_vector(start_node, Node(end_point[0], end_point[1]))
         test_point = np.array([0.0, 0.0])
         for i in range(int(self.rho)):
-            test_point[0] = location_start.locationX + i * u_hat[0]
-            test_point[1] = location_start.locationY + i * u_hat[1]
-            # Check if the test point is outside the grid boundaries
+            test_point[0] = start_node.x + i * u_hat[0]
+            test_point[1] = start_node.y + i * u_hat[1]
             if (test_point[0] < 0 or test_point[0] >= self.grid.shape[1] or test_point[1] < 0 or test_point[1] >= self.grid.shape[0]):
                 return True
             if self.grid[int(round(test_point[1])), int(round(test_point[0]))] == 1:
@@ -93,12 +82,12 @@ class RRTalgo:
         for child in root.children:
             self.find_nearest(child, point)
 
-    def distance(self, node1, point):
-        dist = np.sqrt((node1.locationX - point[0]) ** 2 + (node1.locationY - point[1]) ** 2)
+    def distance(self, node, point):
+        dist = np.sqrt((node.x - point[0]) ** 2 + (node.y - point[1]) ** 2)
         return dist
 
     def goal_found(self, point):
-        if self.distance(self.goal, point) <= self.rho:
+        if self.distance(self.goal_node, point) <= self.rho:
             return True
 
     def reset_nearest_values(self):
@@ -106,10 +95,10 @@ class RRTalgo:
         self.nearest_dist = 10000
 
     def retrace_rrt_path(self, goal):
-        if goal.locationX == self.random_tree.locationX:
+        if goal.x == self.random_tree.x:
             return
         self.num_waypoints += 1
-        current_point = np.array([goal.locationX, goal.locationY])
+        current_point = np.array([goal.x, goal.y])
         self.waypoints.insert(0, current_point)
         self.path_distance += self.rho
         self.retrace_rrt_path(goal.parent)
@@ -122,7 +111,7 @@ num_iterations = 300
 step_size = 60
 
 # Create a visualization of the grid with start and goal points
-start_location = plt.Circle((start[0],start[1]), step_size/2, color = 'c', fill = False )
+start_location = plt.Circle((start[0], start[1]), step_size/2, color='c', fill=False)
 goal_region = plt.Circle((goal[0], goal[1]), step_size, color='b', fill=False)
 fig = plt.figure('RRT Algorithm')
 plt.imshow(grid, cmap='binary')
@@ -136,7 +125,7 @@ plt.ylabel('Y-axis (m)')
 plt.show()
 
 # Initialize the RRT algorithm
-rrt = RRTalgo(start, goal, num_iterations, grid, step_size)
+rrt = RRTAlgorithm(start, goal, num_iterations, grid, step_size)
 
 for i in range(rrt.iterations):
     rrt.reset_nearest_values()
@@ -148,13 +137,13 @@ for i in range(rrt.iterations):
     if not is_in_obstacle:
         rrt.add_child(new_point[0], new_point[1])
         plt.pause(0.1)
-        plt.plot([rrt.nearest_node.locationX, new_point[0]], [rrt.nearest_node.locationY, new_point[1]], 'go', linestyle="--")
+        plt.plot([rrt.nearest_node.x, new_point[0]], [rrt.nearest_node.y, new_point[1]], 'go', linestyle="--")
         if rrt.goal_found(new_point):
             rrt.add_child(goal[0], goal[1])
             print('Goal found!')
             break
 
-rrt.retrace_rrt_path(rrt.goal)
+rrt.retrace_rrt_path(rrt.goal_node)
 rrt.waypoints.insert(0, start)
 print("Number of waypoints:", rrt.num_waypoints)
 print("Path Distance:", rrt.path_distance)
@@ -163,3 +152,4 @@ print("Waypoints:", rrt.waypoints)
 for i in range(len(rrt.waypoints) - 1):
     plt.plot([rrt.waypoints[i][0], rrt.waypoints[i + 1][0]], [rrt.waypoints[i][1], rrt.waypoints[i + 1][1]], 'ro', linestyle='--')
     plt.pause(0.1)
+
